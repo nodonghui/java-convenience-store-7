325762eea1f85ff7facab60f5c4a98761ec804f6
nodonghui <163218508+nodonghui@users.noreply.github.com>
2024-11-08 16:47:11 +0900

    fect(chore): 프로모션 클래스 구현
    프로모션 목록을 저장할수있는 클래스를 구현했습니다.

---
+++ src/main/java/store/promotion/Promotion.java
@@ -0,0 +1,70 @@
+package store.promotion;
+import  java.time.LocalDate;
+public class Promotion {
+    private String name;
+    private int buy;
+    private int get;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Promotion(String name, int buy, int get, LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // Getter and Setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBuy() {
+        return buy;
+    }
+
+    public void setBuy(int buy) {
+        this.buy = buy;
+    }
+
+    public int getGet() {
+        return get;
+    }
+
+    public void setGet(int get) {
+        this.get = get;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    // toString method (디버깅 및 정보 확인용)
+    @Override
+    public String toString() {
+        return "Promotion{" +
+                "name='" + name + '\'' +
+                ", buy=" + buy +
+                ", get=" + get +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+}
